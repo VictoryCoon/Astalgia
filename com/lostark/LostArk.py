8e86d8ed3594e39732b3e89688fff9d06be21063
victorycoon <victorycoon@gmail.com>
2025-05-26 16:44:10 +0900

    스타일 수정 중

--- com/lostark/LostArk.py
+++ com/lostark/LostArk.py
@@ -23,10 +23,13 @@ material_code = os.getenv("MATERIAL_CODE")  # 강화재료
 farming_code = os.getenv("FARMING_CODE")  # 생활재료
 
 # Classifier
-trend_engraving_list = os.getenv("CURRENT_TREND_ENGRAVING_LIST").split("|")
+trend_atk_egv_list = os.getenv("CURRENT_TREND_ATK_EGV_LIST").split("|")
+trend_spt_egv_list = os.getenv("CURRENT_TREND_SPT_EGV_LIST").split("|")
 trend_material_list = os.getenv("CURRENT_TREND_MATERIAL_LIST").split("|")
 trend_enhance_list = os.getenv("CURRENT_TREND_ENHANCE_LIST").split("|")
+trend_scroll_list = os.getenv("CURRENT_TREND_SCROLL_LIST").split("|")
 trend_book_list = os.getenv("CURRENT_TREND_BOOK_LIST").split("|")
+esther = os.getenv("ESTHER").split("|")
 living_flower = os.getenv("FARMING_FLOW_LIST").split("|")
 living_fell = os.getenv("FARMING_FELL_LIST").split("|")
 living_mine = os.getenv("FARMING_MINE_LIST").split("|")
@@ -110,10 +113,15 @@ async def EngravingList(param:Optional[str]=None):
     if wildcard != "":
         for egv in engravings:
             if egv["Name"].find(wildcard) >= 0:
+                egv["Type"] = "99"
                 filtered_engravings.append(egv)
     else:
         for egv in engravings:
-            if egv["Name"] in trend_engraving_list:
+            if egv["Name"] in trend_atk_egv_list:
+                egv["Type"] = "10"
+                filtered_engravings.append(egv)
+            if egv["Name"] in trend_spt_egv_list:
+                egv["Type"] = "20"
                 filtered_engravings.append(egv)
 
     return filtered_engravings
@@ -169,10 +177,24 @@ async def MaterialList(param:Optional[str]=None):
     if wildcard != "":
         for mat in materials:
             if mat["Name"].find(wildcard) >= 0:
+                mat["Type"] = "99"
                 filtered_materials.append(mat)
     else:
         for mat in materials:
-            if mat["Name"] in trend_material_list or mat["Name"] in trend_enhance_list or mat["Name"] in trend_book_list:
+            if mat["Name"] in trend_material_list:
+                mat["Type"] = "10"
+                filtered_materials.append(mat)
+            elif mat["Name"] in trend_enhance_list:
+                mat["Type"] = "11"
+                filtered_materials.append(mat)
+            elif mat["Name"] in trend_scroll_list:
+                mat["Type"] = "12"
+                filtered_materials.append(mat)
+            elif mat["Name"] in trend_book_list:
+                mat["Type"] = "13"
+                filtered_materials.append(mat)
+            elif mat["Name"] in esther:
+                mat["Type"] = "00"
                 filtered_materials.append(mat)
 
     return filtered_materials
@@ -213,10 +235,27 @@ async def FarmingList(param:Optional[str]=None):
     if wildcard != "":
         for frm in farmings:
             if frm["Name"].find(wildcard) >= 0:
+                frm["Type"] = "99"
                 filtered_farmings.append(frm)
     else:
         for frm in farmings:
-            if frm["Name"] in living_flower or frm["Name"] in living_fell or frm["Name"] in living_mine or frm["Name"] in living_hunt or frm["Name"] in living_fish or frm["Name"] in living_arch:
+            if frm["Name"] in living_flower:
+                frm["Type"] = "10"
+                filtered_farmings.append(frm)
+            elif frm["Name"] in living_fell:
+                frm["Type"] = "11"
+                filtered_farmings.append(frm)
+            elif frm["Name"] in living_mine:
+                frm["Type"] = "12"
+                filtered_farmings.append(frm)
+            elif frm["Name"] in living_hunt:
+                frm["Type"] = "13"
+                filtered_farmings.append(frm)
+            elif frm["Name"] in living_fish:
+                frm["Type"] = "14"
+                filtered_farmings.append(frm)
+            elif frm["Name"] in living_arch:
+                frm["Type"] = "15"
                 filtered_farmings.append(frm)
 
     return filtered_farmings
